f429b25b93b678d37e83428240624965dd72edba
Amitpnk <amit.naik8103@gmail.com>
2017-01-17 08:46:45 +0530

    Add files via upload

---
+++ AngularGrid.Model/EmployeeDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularGrid.Model
+{
+    public class EmployeeDetail
+    {
+        public int ID { get; set; }
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public string EmpEmailID { get; set; }
+        public int VirtualItemCount { get; set; }
+    }
+}
+
+
---
+++ AngularGrid.Repository/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngularGrid.Repository
+{
+    public interface IRepository<AnyType>
+    {
+        List<AnyType> List(int PageSize, int PageIndex);
+    }
+}
