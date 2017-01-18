ae2b4cd7662bf62c7b97c8df9cbff0dd80fb00cf
Amit P Naik <amit.naik8103@gmail.com>
2017-01-18 07:28:43 +0530

    Add files via upload

---
+++ AngularGrid/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularGrid.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
